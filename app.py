fb9aeafc9cd5ef817b441a5284643bc7f171f6c0
partycrasher546 <muthonidennis546@gmail.com>
2024-08-19 13:42:43 +0300

    req amend

--- app.py
+++ app.py
@@ -768,4 +768,4 @@ def get_payment_status():
     return jsonify(payment_status)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
